7124a352b46177413b2d095b02ef349f11a47a6a
praveendc <praveenkumar@datacaliper.com>
2025-05-04 14:52:36 +0530

    Initial ai agent scracth code commit

---
+++ main.py
@@ -0,0 +1,64 @@
+from dotenv import load_dotenv
+from pydantic import BaseModel
+from langchain_openai import ChatOpenAI
+from langchain_anthropic import ChatAnthropic
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.output_parsers import PydanticOutputParser
+from langchain.agents import create_tool_calling_agent, AgentExecutor
+from tools import search_tool, wiki_tool, save_tool
+import json
+import re
+
+load_dotenv()
+
+class ResearchResponse(BaseModel):
+    topic: str
+    summary: str
+    sources: list[str]
+    tools_used: list[str]
+    
+
+llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash", temperature=0.7)
+parser = PydanticOutputParser(pydantic_object=ResearchResponse)
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+            You are a research assistant that will help generate a research paper.
+            Answer the user query and use neccessary tools. 
+            Wrap the output in this format and provide no other text\n{format_instructions}
+            """,
+        ),
+        ("placeholder", "{chat_history}"),
+        ("human", "{query}"),
+        ("placeholder", "{agent_scratchpad}"),
+    ]
+).partial(format_instructions=parser.get_format_instructions())
+
+tools = [search_tool, wiki_tool, save_tool]
+agent = create_tool_calling_agent(
+    llm=llm,
+    prompt=prompt,
+    tools=tools
+)
+
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+query = input("What can i help you research? ")
+raw_response = agent_executor.invoke({"query": query})
+
+try:
+    # structured_response = parser.parse(raw_response.get("output")[0]["text"])
+    # print(structured_response)
+    # Extract JSON string from markdown block
+    output = raw_response.get("output", "")
+    json_str = re.search(r"```json\n(.*?)```", output, re.DOTALL)
+    if json_str:
+        structured_response = json.loads(json_str.group(1))
+        print(structured_response)
+    else:
+        print("Could not find JSON block in response.")
+except Exception as e:
+    print("Error parsing response", e, "Raw Response - ", raw_response)
